7f2c265ae33bd9652d4a9b02aa63468862cd7301
gribanovartem <gribanovartem22@gmail.com>
2019-12-16 21:10:57 +0300

    complete checkbox

--- filter/src/App.js
+++ filter/src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import './App.css';
+
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checkbox: false,
+            words: this.props.words,
+        };
+        this.checked= this.checked.bind(this);
+    }
+    checked() {
+        if(this.state.checkbox===false) {
+            let wordsToSorted = JSON.parse(JSON.stringify(this.props.words));// копирование Хэша
+            this.setState({checkbox: true});
+            this.setState({words: wordsToSorted.sort()});
+            console.log(this.props.words);
+        } else {
+            this.setState({checkbox: false});
+            this.setState({words: this.props.words});
+            console.log(this.props.words);
+        }
+    }
     render() {
+        let wordElements = this.state.words.map((item, index)=>
+            <p key={index}>{item}</p>
+        );
         return (
-            <div>gfdhsht</div>
+            <div className='filter'>
+                <input type='checkbox' onClick={this.checked}/>
+                <input type='text' className='textInput'/>
+                <button>Сброс</button><br/>
+                <div className='textArea'>
+                    {wordElements}
+                </div>
+            </div>
         )
     }
 };
--- filter/src/index.js
+++ filter/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+let words = ['краниопластика', 'дибензимидазол', 'бетоноукладчик', 'ультрамикротом', 'дальновидность',
+    'несовершенство', 'ракетомоделист', 'центробежность', 'пенополиуретан', 'автопаразитизм'];
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App words={words}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
