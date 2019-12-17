19e9b1100973a683b1e6f458b626bcb10c1f65d7
gribanovartem <gribanovartem22@gmail.com>
2019-12-17 19:57:43 +0300

    fix discharge button

--- filter/src/App.js
+++ filter/src/App.js
@@ -8,25 +8,28 @@ export default class App extends React.Component {
             checkbox: false,
             words: this.props.words,
             regexp: /[а-яА-Я]*/,
+            textValue: '',
         };
         this.checked= this.checked.bind(this);
         this.changeText= this.changeText.bind(this);
+        this.discharge= this.discharge.bind(this);
     }
     checked() {
         if(this.state.checkbox===false) {
             let wordsToSorted = JSON.parse(JSON.stringify(this.props.words));// копирование Хэша
             this.setState({checkbox: true});
             this.setState({words: wordsToSorted.sort()});
-            console.log(this.props.words);
         } else {
             this.setState({checkbox: false});
             this.setState({words: this.props.words});
-            console.log(this.props.words);
         }
     }
     changeText(EO) {
         let textValue = EO.target.value;
-        this.setState({regexp: new RegExp(textValue)})
+        this.setState({regexp: new RegExp(textValue), textValue: textValue})
+    }
+    discharge() {
+        this.setState({checkbox: false, words: this.props.words, regexp: /[а-яА-Я]*/, textValue: ''});
     }
     render() {
         let wordElements = this.state.words.map((item, index)=> {
@@ -37,9 +40,9 @@ export default class App extends React.Component {
         );
         return (
             <div className='filter'>
-                <input type='checkbox' onClick={this.checked}/>
-                <input type='text' className='textInput' onChange={this.changeText}/>
-                <button>Сброс</button><br/>
+                <input type='checkbox' onClick={this.checked} checked={this.state.checkbox}/>
+                <input type='text' className='textInput' onChange={this.changeText} value={this.state.textValue}/>
+                <button onClick={this.discharge}>Сброс</button><br/>
                 <div className='textArea'>
                     {wordElements}
                 </div>
