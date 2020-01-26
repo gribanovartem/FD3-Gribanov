43de5f6b495cb77d2101ba553768aa90d8976e7e
gribanovartem <gribanovartem22@gmail.com>
2020-01-26 18:38:17 +0300

    add tests

--- mobile/src/Components/__tests__/addFunctional.test.js
+++ mobile/src/Components/__tests__/addFunctional.test.js
@@ -8,22 +8,20 @@ import clients from '../../Clients';
 import ReactTestUtils from 'react-dom/test-utils';
 test('Testing add button in MobileClient', () => {
     const component = renderer.create(<MobileCompany clients={clients}/>);
-    const comp = mount(<MobileCompany clients={clients}/>);
     let componentTree=component.toJSON();
     expect(componentTree).toMatchSnapshot();
-
+    //находим кнопку на клиенте, нажимаем и делаем снимок
     const button = component.root.findByProps({className: 'button addButton'}); 
     button.props.onClick();
     componentTree=component.toJSON();
     expect(componentTree).toMatchSnapshot();
+    //находим инпут с фамилией, меняем значение
     const famInput = component.root.findByProps({className: 'fam'}); 
     famInput.value = 'жираф';
-
     ReactTestUtils.Simulate.change(famInput);
+    //находим кнопку "добавить" в форме, нажимаем и делаем снимок
     const finalButton = component.root.findByProps({className: 'button finalAdd'}); 
-    console.log(famInput);
     finalButton.props.onClick();
-
     componentTree=component.toJSON();
     expect(componentTree).toMatchSnapshot();
 
