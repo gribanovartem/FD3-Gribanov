20f4c2a79b42ffe1882630fb9f0ed7053770db21
gribanovartem <gribanovartem22@gmail.com>
2020-02-14 11:49:27 +0300

    start

--- ShopProject/src/ui/routes/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-
-import CoreLayout from './CoreLayout'
-
-const Routes = () => <Route path="/" component={CoreLayout} />
-
-export default Routes
